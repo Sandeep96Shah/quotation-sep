85a932d97e5408c1dc314a704209c83b39a753ed
Sandeep Shah <sandeep2016shah@gmail.com>
2023-04-13 18:15:49 +0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const database = require('./config/mongoose');
 const passport_jwt = require("./config/passport_jwt");
 
